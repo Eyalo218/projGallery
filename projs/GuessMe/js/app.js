68fee5c20b5062f513862898c6497eed4c69c8d0
Eyal Ohayon <EyalOhayon1@gmail.com>
2018-06-14 00:06:22 +0300

    fixes and tweaks

--- projs/GuessMe/js/app.js
+++ projs/GuessMe/js/app.js
@@ -15,48 +15,10 @@ function startGuessing() {
 }
 
 function renderQuest() {
-    $('.gameQuest>h2').text(gCurrQuest.txt)  // TODO: select the <h2> inside gameQuest and update its text by the currQuest text
+    $('.gameQuest>h2').text(getCurrentQuest().txt) ;
 }
 
-function userResponse(res) {
 
-    // If this node has no children
-    if (isChildless(gCurrQuest)) {
-        if (res === 'yes') {
-            $('.genieTalk').text('Woohoo! I knew im gonna be right ')
-            $('.gameQuest').hide();
-            restartGame();
-        } else {
-            $('.genieTalk').text('Hmm, Please teach me who it was')
-            $('.gameQuest').hide();
-            $('.gameNewQuest').show();
-        }
-    } else {
-        gPrevQuest = gCurrQuest;
-        gCurrQuest= gCurrQuest[res];
-        gLastRes=res;
-        renderQuest();
-    }
-}
-
-function addGuess() {
-    gPrevQuest[gLastRes] = createQuest($('#newQuest').val());
-    gPrevQuest[gLastRes].yes = createQuest($('#newGuess').val());
-    gPrevQuest[gLastRes].no = gCurrQuest;
-    // TODO: create 2 new Quests based on the inputs' values
-    // TODO: connect the 2 Quests to the quetsions tree
-    saveToStorage(getQuests());
-    restartGame();
-}
-
-function createQuest(txt) {
-    return {
-        txt: txt,
-        yes: null,
-        no: null
-    }
-    
-}
 
 function restartGame() {
     $('.gameNewQuest').hide();
--- projs/GuessMe/js/quest-services.js
+++ projs/GuessMe/js/quest-services.js
@@ -19,7 +19,48 @@ function getQuests(){
     return gQuestsTree;
 }
 
+function userResponse(res) {
 
+    // If this node has no children
+    if (isChildless(gCurrQuest)) {
+        if (res === 'yes') {
+            $('.genieTalk').text('Woohoo! I knew im gonna be right ')
+            $('.gameQuest').hide();
+            restartGame();
+        } else {
+            $('.genieTalk').text('Hmm, Please teach me who it was')
+            $('.gameQuest').hide();
+            $('.gameNewQuest').show();
+        }
+    } else {
+        gPrevQuest = gCurrQuest;
+        gCurrQuest= gCurrQuest[res];
+        gLastRes=res;
+        renderQuest();
+    }
+}
+
+function getCurrentQuest(){
+    return gCurrQuest;
+}
+
+function createQuest(txt) {
+    return {
+        txt: txt,
+        yes: null,
+        no: null
+    }
+    
+}
+function addGuess() {
+    gPrevQuest[gLastRes] = createQuest($('#newQuest').val());
+    gPrevQuest[gLastRes].yes = createQuest($('#newGuess').val());
+    gPrevQuest[gLastRes].no = gCurrQuest;
+    // TODO: create 2 new Quests based on the inputs' values
+    // TODO: connect the 2 Quests to the quetsions tree
+    saveToStorage(getQuests());
+    restartGame();
+}
 
 // to put in utils
 function saveToStorage(value) {
